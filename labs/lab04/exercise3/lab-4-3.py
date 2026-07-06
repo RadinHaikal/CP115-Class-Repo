12c13baba5eec07893253612a4360345edd49e71
syazwana2122 <azwnna@gmail.com>
2026-07-06 08:54:02 +0800

    input lab-4-3

---
+++ labs/lab04/exercise3/lab-4-3.py
@@ -0,0 +1,10 @@
+hours = int(input())
+if hours <= 2:
+    parkingFee = 0
+else:
+    if hours <= 5:
+        parkingFee = hours - 2 * 2.0
+    else:
+        parkingFee = 6 + hours - 5 * 3.0
+if parkingFee > 30.0:
+    parkingFee = 30.0

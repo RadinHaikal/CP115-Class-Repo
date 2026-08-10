83e3d07ee36c0e08206c3a99e770638c42006898
syazwana2122 <azwnna@gmail.com>
2026-08-10 09:11:28 +0800

    lab6andtest

--- labs/lab06/exercise1/exercise1.py
+++ labs/lab06/exercise1/exercise1.py
@@ -2,3 +2,15 @@
 # Print the receipt shown in the lab, using \n for new lines and \t for columns.
 # Calculate every total, subtotal, and tax in your code. Do not type the money
 # amounts in directly. Show every amount with exactly two decimal places.
+# Without \n - everything prints on one line
+Coffee_price = 3.50
+Coffee_qty = int(input("Enter the quantity of Coffee: "))
+Muffin_price = 2.10
+Muffin_qty = int(input("Enter the quantity of Muffin: "))
+Water_price = 1.05
+Water_qty = int(input("Enter the quantity of Water: "))
+Total = (Coffee_price * Coffee_qty) + (Muffin_price * Muffin_qty) + (Water_price * Water_qty)
+    Receipt = f"""\n========== Receipt ==========\nItem\tPrice\tQty\tTotal\nCoffee\t$3.50\t{Coffee_qty}\t${Coffee_price * Coffee_qty}\nMuffin\t$2.10\t{Muffin_qty}\t${Muffin_price * Muffin_qty}\nWater\t$1.05\t{Water_qty}\t${Water_price * Water_qty}\n------------------------------\nSubtotal\t\t${Total}\nTax (6%)\t\t${Total * 0.06}\nTotal\t\t${Total * 1.06}\n=============================="""
+    print(Receipt)
+
+    
---
+++ labs/lab06/exercise1/lab-test-2.py
@@ -0,0 +1,9 @@
+#Student Info
+name = str(input("Enter your name: "))
+Matric_No = str(input("Enter your Matriculation Number: "))
+marks = int(input("Enter your marks: "))
+#Arithmetic Expression
+total_marks = 2 * marks
+programme = f"""\n Name: {name}\t\t\t\tMatric. No: {Matric_No}\n\n*\t\t\t*\n**\t\t**\n***\t***\n********\n***\t***\n**\t\t**\n*\t\t\t*\n\n\nThis is my\n\tsecond\n\t\tassignment\nI want 2x10 marks, which is {total_marks} full marks """
+print(programme)
+

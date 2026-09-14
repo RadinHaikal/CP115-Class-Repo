029020a79e0e6554476ae757b8dc8eeee7550bb5
syazwana2122 <azwnna@gmail.com>
2026-09-14 09:33:33 +0800

    Selection

--- labs/lab08/exercise4/exercise4.py
+++ labs/lab08/exercise4/exercise4.py
@@ -1,8 +1,11 @@
 current_reading = int(input())
 previous_reading = int(input())
 
+consumption = current_reading-previous_reading
 
-
+if consumption >= 20:
+    water_cost = 0.57
+elif consumption 
 print(consumption)
 print(water_cost)
 print(total_bill)
